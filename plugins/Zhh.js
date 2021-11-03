fa70c67f47f6a9c21b4d82c3db813ee1c906604b
souravkl12 <93660937+souravkl12@users.noreply.github.com>
2021-11-03 19:33:22 +0530

    Update Zhh.js

--- plugins/Zhh.js
+++ plugins/Zhh.js
@@ -3,7 +3,7 @@ const { WAConnection, MessageOptions, MessageType, Mimetype, Presence } = requir
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const plk1 = `*𝐕𝐄𝐑𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄𝐃*\n\nʏᴏᴜʀ ᴅᴀᴛᴀ ɪɴғᴏ ᴡᴏɴᴛ ʙᴇ sʜᴏᴡɴ ᴘᴜʙʟɪᴄ. ᴛʜɪs ɪs ғᴏʀ ᴜsᴇʀs sᴇᴄᴜʀɪᴛʏ. ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴅᴇᴘʟᴏʏ ʟɪɴᴋ ᴊᴜsᴛ ᴍsɢ - *deploy*\n\n*𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝚉𝙰𝚁𝙰𝙼𝚆𝙾𝙻 💖*`
+const plk1 = "*𝐕𝐄𝐑𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄𝐃*\n\nʏᴏᴜʀ ᴅᴀᴛᴀ ɪɴғᴏ ᴡᴏɴᴛ ʙᴇ sʜᴏᴡɴ ᴘᴜʙʟɪᴄ. ᴛʜɪs ɪs ғᴏʀ ᴜsᴇʀs sᴇᴄᴜʀɪᴛʏ. ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴅᴇᴘʟᴏʏ ʟɪɴᴋ ᴊᴜsᴛ ᴍsɢ - *deploy*\n\n*𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝚉𝙰𝚁𝙰𝙼𝚆𝙾𝙻 💖*"
 const plk = "*🛑 𝔻𝕠 𝕟𝕠𝕥 𝕣𝕖𝕡𝕝𝕪. 𝕋𝕙𝕚𝕤 𝕓𝕠𝕥 𝕚𝕤 𝕡𝕣𝕚𝕧𝕒𝕥𝕖 🛑*"
 const ll = "```https://bit.ly/deploy-ZaraMwol```"
 
